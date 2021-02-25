ae6c54bceba35e8078af0df152a348eb76f9f057
Geovanni Perez <1775792+geoperez@users.noreply.github.com>
2021-02-25 08:44:57 -0600

    Update bump.sh

--- bump.sh
+++ bump.sh
@@ -1,2 +1,2 @@
 version=$(node -e "console.log(require('./package.json').version)")
-pm version --no-git-tag-version $(semver -i $level $version)
+npm version --no-git-tag-version $(semver -i $level $version)
