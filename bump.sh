4efba337e0717360bf3bb353e94c341e015b1de8
Juan Pablo Alcalá <32104554+jpalcala@users.noreply.github.com>
2023-06-30 13:26:26 -0600

    Update bump.sh

--- bump.sh
+++ bump.sh
@@ -1,2 +1,2 @@
-version=$(node -e "console.log(require('./'$route'package.json').version)")
+version=$(node -e "console.log(require('./$route\package.json').version)")
 npm version --no-git-tag-version $(semver -i $level $version)
